2db214d00933874e3373674fea43c6ebd3406d52
Jimmy <jimmy@reslv.io>
2020-08-12 10:59:56 +0800

    show version in index page fix #5

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const pkg = require("../package.json");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Express", version: pkg.version });
 });
 
 module.exports = router;
